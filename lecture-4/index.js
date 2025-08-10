5e56ba969f03e17a0d961b39b1fff9aa60afaa4f
Ayush <bhandariayush935@gmail.com>
2025-08-10 23:36:35 +0530

    updating server file

--- lecture-4/index.js
+++ lecture-4/index.js
@@ -5,11 +5,24 @@ const PORT = 2000
 const users = require('./MOCK_DATA.json')
 
 // Middleware - plugins
-app.use(express.urlencoded({extended : false}))
+app.use(express.urlencoded({ extended: false }))
+
+// Middleware from scratch
+app.use((req, res, next) => {
+    const now = new Date();
+    const dateStr = `${now.getDate()}-${now.getMonth() + 1}-${now.getFullYear()}`;
+    const time = `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
+    fs.appendFile('./logs.txt',
+        `\nRequest received on ${dateStr} at ${time} , path :- ${req.path}\n`
+        , (err, data) => {
+            next()
+        })
+})
+
 
 app.get('/users', (req, res) => {
     const html =
-    `
+        `
     <ul> 
         ${users.map(user => `<li> ${user.first_name} </li>`).join("")}
     </ul>
@@ -22,34 +35,33 @@ app.get('/api/users', (req, res) => {
     return res.json(users)
 })
 
-
 app
-.route('/api/users/:id')
-.get((req , res) =>{
-    const id = Number(req.params.id)
-    const user = users.find(user => user.id === id)
-    return res.json(user)
-})
-.put((req,res) =>{
-    // ToDo : Create a new user
-    return res.json({status : "Pending"})
-})
-.patch((req,res) =>{
-    // ToDo : Updating an existing user
-    return res.json({status : "Pending"})
-})
-.delete((req,res) =>{
-    // ToDo : Delete an user
-    return res.json({status : "Pending"})
-})
+    .route('/api/users/:id')
+    .get((req, res) => {
+        const id = Number(req.params.id)
+        const user = users.find(user => user.id === id)
+        return res.json(user)
+    })
+    .put((req, res) => {
+        // ToDo : Create a new user
+        return res.json({ status: "Pending" })
+    })
+    .patch((req, res) => {
+        // ToDo : Updating an existing user
+        return res.json({ status: "Pending" })
+    })
+    .delete((req, res) => {
+        // ToDo : Delete an user
+        return res.json({ status: "Pending" })
+    })
 
-app.post('/api/users/:id' , (req ,res) =>{
+app.post('/api/users/:id', (req, res) => {
     // ToDo : Create a new user
     const body = req.body
-    users.push({...body , id : users.length + 1} )
+    users.push({ ...body, id: users.length + 1 })
     console.log(body)
-    fs.writeFile('./MOCK_DATA.json' , JSON.stringify(users) , (err ,data) =>{
-        return res.json({status : "Success" , id : users.length})
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
+        return res.json({ status: "Success", id: users.length })
 
     })
 })
