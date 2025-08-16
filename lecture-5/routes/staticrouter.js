d17ad84a7a771ee690a83129111d16d0f5acf226
Ayush <bhandariayush935@gmail.com>
2025-08-16 23:07:50 +0530

    Rendering UI router

--- lecture-5/routes/staticrouter.js
+++ lecture-5/routes/staticrouter.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const router  = express.Router();
-const URL = require('../models/url')
+const URL = require('../models/urlmodel')
 
 router.get('/' , async (req , res) =>{
     const allurls = await URL.find({})
     return res.render('home' , { urls : allurls})
+})
 
+router.get('/signup' , (req,res) =>{
+    res.render('signup')
 })
 
 module.exports = router;
