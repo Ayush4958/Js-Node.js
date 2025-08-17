3d02e9c4608867e7c11d64d15cbe7802edc50a8d
Ayush <bhandariayush935@gmail.com>
2025-08-18 00:17:30 +0530

    Static Serving

--- lecture-5/routes/staticrouter.js
+++ lecture-5/routes/staticrouter.js
@@ -3,12 +3,19 @@ const router  = express.Router();
 const URL = require('../models/urlmodel')
 
 router.get('/' , async (req , res) =>{
-    const allurls = await URL.find({})
-    return res.render('home' , { urls : allurls})
+    if (!req.user) return res.redirect('/login')
+    const allurls = await URL.find({ 
+        createdBy : req.user._id
+    })
+    return res.render('home' , { urls : allurls});
 })
 
 router.get('/signup' , (req,res) =>{
-    res.render('signup')
+    res.render('signup');
+})
+
+router.get('/login' , (req , res) =>{
+    res.render('login');
 })
 
 module.exports = router;
