a42ff90cdb9ef5c8ae1467ad93818b18044674bb
Ayush <bhandariayush935@gmail.com>
2025-08-19 23:18:49 +0530

    middleware updated

--- lecture-5/middleware/auth.js
+++ lecture-5/middleware/auth.js
@@ -1,22 +1,28 @@
-const {getUser} = require('../controllers/userFunc')
+const { get } = require('mongoose');
+const {getUser} = require('../service/auth')
 
-const forLoggedinUser = (req , res , next) =>{
-    const useruid = req.cookies.uid
-    if (!useruid) return res.redirect('/login')
-    const user = getUser(useruid)
-    if (!user) return res.redirect('/login')
-    req.user = user
+
+const funcForAuthorization = ( req , res , next) =>{
+    const tokenCookie = req.cookie?.token;
+    req.user = null;
+
+    const token = tokenCookie
+    const user = getUser(token)
+
+    req.user = user;
     next();
 }
 
-const checkauth = async (req , res , next) =>{
-    const useruid = req.cookies.uid
-    const user = getUser(useruid)
-    req.user = user
-    next();
+const restrictrole = (roles) =>{
+    return (req , res , next) =>{
+        if (!req.user) return res.redirect('/login')
+        if (!roles.includes(req.user.role)) return res.end("UnAuthorized")
+            next()
+    }
 }
+
 
 module.exports = {
-    forLoggedinUser , 
-    checkauth
+    funcForAuthorization,
+    restrictrole
 }
