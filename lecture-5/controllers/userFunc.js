d2b05f19d8dcc57a55b289188ef605ac0d30c2c5
Ayush <bhandariayush935@gmail.com>
2025-08-20 20:46:01 +0530

    User Function holder

--- lecture-5/controllers/userFunc.js
+++ lecture-5/controllers/userFunc.js
@@ -1,38 +1,27 @@
 const { v4: uuidv4 } = require('uuid');
 const User = require('../models/usermodel');
-
-// Session Management
-const sessionIdToUserMap = new Map();
-const setUser = (id, user) => {
-    sessionIdToUserMap.set(id, user);
-}
-const getUser = (id) => {
-    return sessionIdToUserMap.get(id);
-}
+const {setUser , getUser} = require('../service/auth')
 
 // function for creating a User
 const createUser = async (req, res) => {
-    const { user, mail, pass } = req.body;
+    const { user, mail, pass } = req.body
     await User.create({ user, mail, pass })
     return res.redirect("/")
 }
 
 // function for checking a User
 const checkuser = async (req, res) => {
-    const { mail, pass } = req.body;
-    const user = await User.findOne({ mail, pass })
+    const { mail, pass } = req.body
+    const user = await User.findOne({ mail , pass })
     if (!user) {
         return res.render("login", { error: "Invalid Credentials" })
     }
-    const sessionId = uuidv4();
-    setUser(sessionId, user)
-    res.cookie("uid" , sessionId)
-    return res.redirect("/")
+    const token = setUser(user)
+    res.cookie('token' , token)
+    return res.render("home")
 }
  
 module.exports = {
     createUser,
     checkuser,
-    setUser,
-    getUser,
 }
