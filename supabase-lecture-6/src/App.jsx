e05c56325e963bfeee9852a58afbf35233857487
Ayush <bhandariayush935@gmail.com>
2025-08-31 22:56:51 +0530

    authorization handling

--- supabase-lecture-6/src/App.jsx
+++ supabase-lecture-6/src/App.jsx
@@ -1,11 +1,36 @@
 import { useEffect, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
 import TaskManager from "./components/taskbar"
+import Auth from "./components/auth";
+import { supabase } from "./supabaseclient";
 
 function App() {
 
+  const [session , setsession] = useState(false)
+
+  const fetchsession = async () =>{
+    const currentsession = await supabase.auth.getSession();
+    console.log(currentsession)
+    setsession(currentsession.data.session)
+  }
+
+  useEffect(() =>{
+    fetchsession()
+  }, [])
+
+  const logout = async() =>{
+    const { error } = await supabase.auth.signOut();
+    if(error){
+      console.log("error while logging out" , error)
+    }
+  }
+
   return (
-    <TaskManager />
+    <>
+
+{session ? <> <button onClick={logout} className="px-3 py-1 items-center justify-center text-sm bg-red-600 text-white rounded hover:bg-red-700 ">Log Out</button> <TaskManager/> </>: <Auth/>}
+
+    </>
   );
 }
 
